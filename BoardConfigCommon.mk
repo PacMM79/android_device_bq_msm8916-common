ec4e615956337b2e2192339ab8fbaceb85d57b7c
Fran P <sweetydesigns@gmail.com>
2020-01-14 20:50:01 +0100

    msm8916-common: Add TARGET_CPU_CORTEX_A53
    + TARGET_CPU_SMP := true
    
    Testing

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,8 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
 
 # Kernel
