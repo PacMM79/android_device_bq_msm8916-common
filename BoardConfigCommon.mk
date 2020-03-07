fd3bee469bfa7539e4c3079b7055bb0732c1fca2
Fran P <sweetydesigns@gmail.com>
2020-03-07 18:55:52 +0100

    msm8916-common: Set cpu "runtime variant" to cortex-a53
    This makes bionic and various art tools that compile/runs cpu-specific code choose the cortex-a53 variant at runtime. It does not affect build-time compilation.
    
    ref: https://gitea.jenslody.de/jens/android_device_samsung_msm8916-common/commit/31190c62b54f78dd97a360e78ddcba571b9685c0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,6 +31,7 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
