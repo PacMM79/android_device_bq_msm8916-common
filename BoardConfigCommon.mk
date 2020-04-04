385ed2f7b46596cd865e132b9f8cca6e5e322203
Fran P <sweetydesigns@gmail.com>
2020-04-05 00:39:18 +0200

    msm8916-common: Switch kernel GCC to Linaro 7.5
    From: https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-eabi/

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,7 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin/arm-eabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-linaro-7.5/bin/arm-eabi-
 
 # Initial API Level of the Device
 PRODUCT_SHIPPING_API_LEVEL := 23
