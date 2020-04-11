9e1b25f18d0388db5b332079245f933639c72c22
Fran P <sweetydesigns@gmail.com>
2020-04-11 04:53:01 +0200

    msm8916-common: Update GPS config
    ref: https://github.com/Harpia-development/device_motorola_msm8916-common/blob/pie-los/msm8916.mk

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,3 +1,6 @@
+ifneq ($(BUILD_TINY_ANDROID),true)
+#Compile this library only for builds with the latest modem image
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -57,8 +60,11 @@ LOCAL_COPY_HEADERS:= \
 
 LOCAL_MODULE := libgps.utils
 LOCAL_MODULE_OWNER := qcom
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
+endif # not BUILD_TINY_ANDROID
--- msm8916.mk
+++ msm8916.mk
@@ -146,15 +146,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service \
-    gps.msm8916 \
-    libshims_get_process_name \
-    libshims_flp
+    gps.msm8916
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
