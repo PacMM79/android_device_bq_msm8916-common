fb9c2f045d84ccf24d4281a0905bae1399da8597
Fran Palmero <sweetydesigns@gmail.com>
2020-06-07 02:05:41 +0200

    msm8916-common: Add Trust service

--- msm8916.mk
+++ msm8916.mk
@@ -332,6 +332,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.bq_8916
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Usb
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.bq_8916
