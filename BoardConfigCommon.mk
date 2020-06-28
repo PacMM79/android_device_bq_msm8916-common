86ea40a4a98e870e0b8d5a45ddffcd044314e784
Fran Palmero <sweetydesigns@gmail.com>
2020-06-28 03:36:20 +0200

    msm8916-common: SELinux Enforce

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,6 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET     := 0x01000000
