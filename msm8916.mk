9c08333797a2da1c83f99d0f3eef8227d49dcb47
Fran Palmero <sweetydesigns@gmail.com>
2020-07-02 19:39:23 +0200

    msm8916-common: Drop thermal service

--- msm8916.mk
+++ msm8916.mk
@@ -326,10 +326,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service
-
 # Time
 PRODUCT_PACKAGES += \
     timekeep \
