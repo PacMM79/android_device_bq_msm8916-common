61cdd588b16478c1a8c2a50a42a88a7870c8aa74
Fran P <sweetydesigns@gmail.com>
2021-02-20 17:50:30 +0100

    msm8916-common: Add android.hardware.location.gps.xml framework

--- msm8916.mk
+++ msm8916.mk
@@ -196,6 +196,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
